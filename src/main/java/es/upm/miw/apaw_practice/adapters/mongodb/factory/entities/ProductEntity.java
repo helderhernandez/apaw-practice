d0f5aa475914fa6a795c997eb20e487dee9dabac
Andrea Calvo <andreac.3108@gmail.com>
2020-09-30 19:47:38 +0200

    add attributes & initialize framework & create getters and setters in ProductEntity #20

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/factory/entities/ProductEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/factory/entities/ProductEntity.java
@@ -12,4 +12,33 @@ public class ProductEntity {
     private String id;
     private BigDecimal wholesalePrice;
     private String description;
+
+    public ProductEntity() {
+        //empty from framework
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getWholesalePrice() {
+        return wholesalePrice;
+    }
+
+    public void setWholesalePrice(BigDecimal wholesalePrice) {
+        this.wholesalePrice = wholesalePrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
