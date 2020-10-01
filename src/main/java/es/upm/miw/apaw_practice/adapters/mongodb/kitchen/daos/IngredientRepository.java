d1fb85c410c0e17ad86b303d75fc19d91a156059
MRxRafi <rafadriveclase@gmail.com>
2020-10-01 14:05:48 +0200

    Added some things into IngredientEntityRepositoryIT. Partial #15

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/kitchen/daos/IngredientRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/kitchen/daos/IngredientRepository.java
@@ -3,5 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.kitchen.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.kitchen.entities.IngredientEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends MongoRepository<IngredientEntity, String> {
+    Optional<IngredientEntity> findByName(String name);
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/kitchen/daos/IngredientEntityRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/kitchen/daos/IngredientEntityRepositoryIT.java
@@ -1,4 +1,23 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.kitchen.daos;
 
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.kitchen.entities.IngredientEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestConfig
 public class IngredientEntityRepositoryIT {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Test
+    void testFindByName(){
+        assertTrue(this.ingredientRepository.findByName("Cacao en polvo").isPresent());
+        IngredientEntity ingredient = this.ingredientRepository.findByName("Cacao en polvo").get();
+
+    }
 }
