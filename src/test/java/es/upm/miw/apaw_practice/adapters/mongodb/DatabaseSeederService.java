02ac7411fc2d0ac11cd304caf39d613265a7bcfe
MRxRafi <rafadriveclase@gmail.com>
2020-10-01 13:53:35 +0200

    Added kitchenSeederService into DatabaseSeederService #15

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.factory.FactorySeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.kitchen.KitchenSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,22 +11,27 @@ public class DatabaseSeederService {
 
     private ShopSeederService shopSeederService;
     private FactorySeederService factorySeederService;
+    private KitchenSeederService kitchenSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, FactorySeederService factorySeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, FactorySeederService factorySeederService,
+                                 KitchenSeederService kitchenSeederService) {
         this.shopSeederService = shopSeederService;
         this.factorySeederService = factorySeederService;
+        this.kitchenSeederService = kitchenSeederService;
         this.seedDatabase();
     }
 
     public void seedDatabase() {
         this.shopSeederService.seedDatabase();
         this.factorySeederService.seedDatabase();
+        this.kitchenSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
         this.factorySeederService.deleteAll();
+        this.kitchenSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
