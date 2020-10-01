62ee21e8b85e206498184dd00b115ef55e3e76be
Pablo <pabloguijarro96@gmail.com>
2020-10-01 23:53:05 +0200

    Conflict resolved #14

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -3,6 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb;
 import es.upm.miw.apaw_practice.adapters.mongodb.factory.FactorySeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.school.SchoolSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.bank.BankSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.movie.MovieSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,15 @@ public class DatabaseSeederService {
     private ShopSeederService shopSeederService;
     private SchoolSeederService schoolSeederService;
     private FactorySeederService factorySeederService;
-
+    private BankSeederService bankSeederService;
+    private MovieSeederService movieSeederService;
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, SchoolSeederService schoolSeederService, FactorySeederService factorySeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, SchoolSeederService schoolSeederService, FactorySeederService factorySeederService, BankSeederService bankSeederService, MovieSeederService movieSeederService) {
         this.shopSeederService = shopSeederService;
         this.schoolSeederService = schoolSeederService;
         this.factorySeederService = factorySeederService;
+        this.bankSeederService = bankSeederService;
+        this.movieSeederService = movieSeederService;
         this.seedDatabase();
     }
 
@@ -25,11 +30,16 @@ public class DatabaseSeederService {
         this.shopSeederService.seedDatabase();
         this.schoolSeederService.seedDatabase();
         this.factorySeederService.seedDatabase();
+        this.bankSeederService.seedDatabase();
+        this.movieSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
+        this.schoolSeederService.deleteAll();
         this.factorySeederService.deleteAll();
+        this.bankSeederService.deleteAll();
+        this.movieSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
