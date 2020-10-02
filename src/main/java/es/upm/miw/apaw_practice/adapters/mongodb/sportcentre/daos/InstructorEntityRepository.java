8e70e43fe2567d58708a3e851bcd0212cdf906e0
aleandrorsanchez <aleeexr7@gmail.com>
2020-10-02 12:24:10 +0200

    InstructorEntity and Repository created #16

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/sportcentre/daos/InstructorEntityRepository.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.daos;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.entities.InstructorEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface InstructorEntityRepository extends MongoRepository<InstructorEntity, String> {
+
+}
