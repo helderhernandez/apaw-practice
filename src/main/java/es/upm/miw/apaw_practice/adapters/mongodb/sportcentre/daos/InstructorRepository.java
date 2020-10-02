41fb45c32784c2a27663ec2bc9b91dd343dca3b2
aleandrorsanchez <aleeexr7@gmail.com>
2020-10-02 12:45:29 +0200

    InstructorEntityRepositoryIT implemented #16

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/sportcentre/daos/InstructorRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/sportcentre/daos/InstructorRepository.java
@@ -3,6 +3,6 @@ package es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.entities.InstructorEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface InstructorEntityRepository extends MongoRepository<InstructorEntity, String> {
+public interface InstructorRepository extends MongoRepository<InstructorEntity, String> {
 
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/sportcentre/SportCentreSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/sportcentre/SportCentreSeederService.java
@@ -1,14 +1,18 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.sportcentre;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.daos.AssistantRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.daos.InstructorRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.daos.SpecialityRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.entities.AssistantEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.entities.InstructorEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.entities.SpecialityEntity;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class SportCentreSeederService {
@@ -17,14 +21,16 @@ public class SportCentreSeederService {
     private AssistantRepository assistantRepository;
     @Autowired
     private SpecialityRepository specialityRepository;
+    @Autowired
+    private InstructorRepository instructorRepository;
 
     public void seedDatabase() {
         LogManager.getLogger(this.getClass()).warn("------- Shop Initial Load -----------");
         AssistantEntity[] assistants = {
                 new AssistantEntity("100", "Alejandro", "Rodriguez", 111111111),
-                new AssistantEntity("101", "Luis", "Perez", 122222222),
+                new AssistantEntity("101", "Julia", "Perez", 122222222),
                 new AssistantEntity("102", "Jose", "Gonzalez", 133333333),
-                new AssistantEntity("103", "Victor", "Castro", 144444444),
+                new AssistantEntity("103", "Elena", "Castro", 144444444),
                 new AssistantEntity("104", "Adrian", "Garcia", 155555555)
         };
         this.assistantRepository.saveAll(Arrays.asList(assistants));
@@ -36,6 +42,14 @@ public class SportCentreSeederService {
                 new SpecialityEntity("spec5", "Full Body", "Full body session for improving the overall shape", 50, false)
         };
         this.specialityRepository.saveAll(Arrays.asList(specialities));
+        InstructorEntity[] instructors = {
+                new InstructorEntity("900", "Javier", "Rodriguez", BigDecimal.valueOf(1000), List.of(specialities[0], specialities[2])),
+                new InstructorEntity("901", "Gonzalo", "Sanchez", BigDecimal.valueOf(900), List.of(specialities[1])),
+                new InstructorEntity("902", "Maria", "Jurado", BigDecimal.valueOf(1200), List.of(specialities[0], specialities[2], specialities[4])),
+                new InstructorEntity("903", "Javier", "Fernandez", BigDecimal.valueOf(1200), List.of(specialities[1], specialities[2], specialities[3])),
+                new InstructorEntity("904", "Teresa", "Izquierdo", BigDecimal.valueOf(1000), List.of(specialities[3], specialities[4])),
+        };
+        this.instructorRepository.saveAll(Arrays.asList(instructors));
 
     }
 
