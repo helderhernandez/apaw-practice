4968db6353b07a2dd88e59336e78c4e04590cc63
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2020-10-02 19:15:36 +0200

    GET readAll() FilmDirector #27

--- src/main/java/es/upm/miw/apaw_practice/domain/models/movie/FilmDirector.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/movie/FilmDirector.java
@@ -1,26 +1,13 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.movie.entities;
+package es.upm.miw.apaw_practice.domain.models.movie;
 
-import es.upm.miw.apaw_practice.domain.models.movie.FilmDirector;
-import org.springframework.beans.BeanUtils;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document
-public class FilmDirectorEntity {
-    @Id
+public class FilmDirector {
     private String id;
     private String name;
     private String fullName;
     private Integer age;
 
-    public FilmDirectorEntity() {
+    public FilmDirector() {
         //empty from framework
-    }
-
-    public FilmDirectorEntity(String name, String fullName, int age) {
-        this.name = name;
-        this.fullName = fullName;
-        this.age = age;
     }
 
     public String getId() {
@@ -51,19 +38,13 @@ public class FilmDirectorEntity {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
-    }
-
-    public FilmDirector toFilmDirector() {
-        FilmDirector filmDirector = new FilmDirector();
-        BeanUtils.copyProperties(this, filmDirector);
-        return filmDirector;
     }
 
     @Override
     public String toString() {
-        return "FilmDirectorEntity{" +
+        return "FilmDirector{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", fullName='" + fullName + '\'' +
