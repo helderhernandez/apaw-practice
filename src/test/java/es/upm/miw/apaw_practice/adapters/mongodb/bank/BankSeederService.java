258c4168fefd960f6c1f684be6a21667971456b6
ulises <ulises.mateo.ortega@alumnos.upm.es>
2020-10-03 21:02:31 +0200

    Added Repositories of Bank and their tests #19

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/BankSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/BankSeederService.java
@@ -1,13 +1,15 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.bank;
 
 
-import es.upm.miw.apaw_practice.adapters.mongodb.bank.daos.CustomerRepository;
-import es.upm.miw.apaw_practice.adapters.mongodb.bank.entities.CustomerEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.bank.daos.*;
+import es.upm.miw.apaw_practice.adapters.mongodb.bank.entities.*;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 
 @Service
@@ -15,6 +17,15 @@ public class BankSeederService {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private MortgageRepository mortgageRepository;
+    @Autowired
+    private SharedAccountsRepository sharedAccountsRepository;
+    @Autowired
+    private ShareholderRepository shareholderRepository;
+
 
     public void seedDatabase() {
         LogManager.getLogger(this.getClass()).warn("------- Bank Initial Load -----------");
@@ -26,9 +37,48 @@ public class BankSeederService {
                 new CustomerEntity("Emma", "666511511J", "666777333", "emmatega@email.com"),
         };
         this.customerRepository.saveAll(Arrays.asList(customers));
+
+        AccountEntity[] accounts = {
+                new AccountEntity(new BigDecimal("68.54"), customers[0], "ES66 987651234"),
+                new AccountEntity(new BigDecimal("4565.23"), customers[1], "ES66 123459876"),
+                new AccountEntity(new BigDecimal("100.00"), customers[2], "ES66 365478912"),
+                new AccountEntity(new BigDecimal("7000.50"), customers[3], "ES66 865973214")
+        };
+        this.accountRepository.saveAll(Arrays.asList(accounts));
+
+        MortgageEntity[] mortgages = {
+                new MortgageEntity(new BigDecimal("5000.00"), new BigDecimal("200.00"), customers[0]),
+                new MortgageEntity(new BigDecimal("60000.00"), new BigDecimal("50300.00"), customers[4]),
+                new MortgageEntity(new BigDecimal("1000.00"), BigDecimal.ZERO, customers[2]),
+                new MortgageEntity(new BigDecimal("2000.00"), BigDecimal.ZERO, customers[3])
+        };
+        this.mortgageRepository.saveAll(Arrays.asList(mortgages));
+
+        SharedAccountEntity[] sharedAccounts = {
+                new SharedAccountEntity(new BigDecimal("4000.00"), "Family", "ES66 123123123",
+                        List.of(customers[0], customers[1])),
+                new SharedAccountEntity(new BigDecimal("20000.00"), "Enterprise", "ES66 321321321",
+                        List.of(customers[0], customers[2], customers[4])),
+                new SharedAccountEntity(new BigDecimal("100"), "Family", "ES66 256652256",
+                        List.of(customers[1], customers[3]))
+        };
+        this.sharedAccountsRepository.saveAll(Arrays.asList(sharedAccounts));
+
+        ShareholderEntity[] shareholders = {
+                new ShareholderEntity(new BigDecimal("20.25"), new BigDecimal("1.22"),
+                        List.of(customers[0], customers[1], customers[4])),
+                new ShareholderEntity(new BigDecimal("79.75"), new BigDecimal("2.33"),
+                        List.of(customers[2], customers[3]))
+        };
+        this.shareholderRepository.saveAll(Arrays.asList(shareholders));
+
     }
 
     public void deleteAll() {
+        this.shareholderRepository.deleteAll();
+        this.sharedAccountsRepository.deleteAll();
+        this.mortgageRepository.deleteAll();
+        this.accountRepository.deleteAll();
         this.customerRepository.deleteAll();
     }
 }
---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/daos/SharedAccountEntityRepositoryIT.java
@@ -0,0 +1,43 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.bank.daos;
+
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.bank.entities.SharedAccountEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@TestConfig
+public class SharedAccountEntityRepositoryIT {
+
+    @Autowired
+    private SharedAccountsRepository sharedAccountsRepository;
+
+    @Test
+    void testFindByIBAN() {
+        assertTrue(this.sharedAccountsRepository.findByIBAN("ES66 123123123").isPresent());
+        SharedAccountEntity sharedAccount = this.sharedAccountsRepository.findByIBAN("ES66 123123123").get();
+        assertTrue(sharedAccount.getAmount().equals(new BigDecimal("4000.00")));
+    }
+
+    @Test
+    void testCreateAndRead() {
+        assertTrue(this.sharedAccountsRepository.findAll().stream()
+                .anyMatch(shared ->
+                        new BigDecimal("4000.00").equals(shared.getAmount()) &&
+                                "Family".equals(shared.getType()) &&
+                                shared.getId() != null &&
+                                2 == shared.getCustomerEntities().size() &&
+                                "Ulises".equals(shared.getCustomerEntities().get(0).getName()) &&
+                                "511511511L".equals(shared.getCustomerEntities().get(0).getDNI()) &&
+                                "666777888".equals(shared.getCustomerEntities().get(0).getPhone()) &&
+                                "ulises@email.com".equals(shared.getCustomerEntities().get(0).getEmail())
+                )
+        );
+    }
+
+
+}
