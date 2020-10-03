16c4c4ac3f0a5889f6ebaf4995488a6894b91bf7
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2020-10-03 21:58:31 +0200

    POST create() method for a Movie #28

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/movie/MoviePersistence.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.movie;
+
+import es.upm.miw.apaw_practice.domain.models.movie.Movie;
+import es.upm.miw.apaw_practice.domain.models.movie.MovieCreation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MoviePersistence {
+    Movie create(MovieCreation movieCreation);
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/movie/MovieService.java
@@ -0,0 +1,22 @@
+package es.upm.miw.apaw_practice.domain.services.movie;
+
+import es.upm.miw.apaw_practice.domain.models.movie.Movie;
+import es.upm.miw.apaw_practice.domain.models.movie.MovieCreation;
+import es.upm.miw.apaw_practice.domain.persistence_ports.movie.MoviePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+    private MoviePersistence moviePersistence;
+
+    @Autowired
+    public MovieService(MoviePersistence moviePersistence) {
+        this.moviePersistence = moviePersistence;
+    }
+
+    public Movie create(MovieCreation movieCreation) {
+        return this.moviePersistence.create(movieCreation);
+    }
+}
