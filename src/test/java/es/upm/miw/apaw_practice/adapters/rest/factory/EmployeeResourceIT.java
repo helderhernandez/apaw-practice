18e0f7b38ab01376715949ebbe280ce87d319673
Andrea Calvo <andreac.3108@gmail.com>
2020-10-03 19:45:18 +0200

    create EmployeeResourceIT tests #33

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/factory/EmployeeResourceIT.java
@@ -0,0 +1,40 @@
+package es.upm.miw.apaw_practice.adapters.rest.factory;
+
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.adapters.rest.shop.ArticleResource;
+import es.upm.miw.apaw_practice.domain.models.factory.Employee;
+import es.upm.miw.apaw_practice.domain.models.factory.EmployeeCreation;
+import es.upm.miw.apaw_practice.domain.models.shop.Article;
+import es.upm.miw.apaw_practice.domain.models.shop.ArticleCreation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RestTestConfig
+public class EmployeeResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testCreate() {
+        EmployeeCreation employeeCreation =
+                new EmployeeCreation("55555555F", "Paula", "Calvo", 666666665L,
+                        new BigDecimal("24000"), "Sales");
+        this.webTestClient
+                .post()
+                .uri(EmployeeResource.EMPLOYEES)
+                .body(BodyInserters.fromValue(employeeCreation))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Employee.class)
+                .value(Assertions::assertNotNull)
+                .value(employeeData -> assertNotNull(employeeData.getId()));
+    }
+}
