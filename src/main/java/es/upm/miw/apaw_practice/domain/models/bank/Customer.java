0fa317f61e664305154afc3fdb034ff2c0e12dda
ulises <ulises.mateo.ortega@alumnos.upm.es>
2020-10-04 14:46:05 +0200

    Add GET #19

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/bank/Customer.java
@@ -0,0 +1,64 @@
+package es.upm.miw.apaw_practice.domain.models.bank;
+
+public class Customer {
+    private String id;
+    private String name;
+    private String DNI;
+    private String phone;
+    private String email;
+
+    public Customer() {
+        //Empty for frameworkk
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", DNI='" + DNI + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
