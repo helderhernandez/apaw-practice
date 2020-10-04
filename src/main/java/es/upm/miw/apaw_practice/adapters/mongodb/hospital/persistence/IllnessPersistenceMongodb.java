817b0fd9543da281d72baa41eff19bc9b3dd10c9
Andrea <drealvarez97@gmail.com>
2020-10-04 20:31:16 +0200

    Added GET readAll and test of Illness #49

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/hospital/persistence/IllnessPersistenceMongodb.java
@@ -0,0 +1,27 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.hospital.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.hospital.daos.IllnessRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.hospital.entities.IllnessEntity;
+import es.upm.miw.apaw_practice.domain.models.hospital.Illness;
+import es.upm.miw.apaw_practice.domain.persistence_ports.hospital.IllnessPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
+
+@Repository("illnessPersistence")
+public class IllnessPersistenceMongodb implements IllnessPersistence {
+
+    private IllnessRepository illnessRepository;
+
+    @Autowired
+    public IllnessPersistenceMongodb(IllnessRepository illnessRepository){
+        this.illnessRepository=illnessRepository;
+    }
+
+    @Override
+    public Stream<Illness> readAll() {
+        return this.illnessRepository.findAll().stream()
+                .map(IllnessEntity::toIllness);
+    }
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/hospital/IllnessService.java
@@ -0,0 +1,22 @@
+package es.upm.miw.apaw_practice.domain.services.hospital;
+
+import es.upm.miw.apaw_practice.domain.models.hospital.Illness;
+import es.upm.miw.apaw_practice.domain.persistence_ports.hospital.IllnessPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Service
+public class IllnessService {
+    private IllnessPersistence illnessPersistence;
+
+    @Autowired
+    public IllnessService(IllnessPersistence illnessPersistence){
+        this.illnessPersistence=illnessPersistence;
+    }
+
+    public Stream<Illness> readAll(){
+        return this.illnessPersistence.readAll();
+    }
+}
