8534d4c6c12834ffdfa78e283b6a3f1e88bc2846
Viviana Castillo <v.castillo@alumnos.upm.es>
2020-10-06 01:31:39 +0200

    TransitTaxesSeederService #87

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -12,6 +12,7 @@ import es.upm.miw.apaw_practice.adapters.mongodb.school.SchoolSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.sportcentre.SportCentreSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.ticketbus.TicketBusSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.transitTaxes.TransitTaxesSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.veterinary.VeterinarySeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,7 @@ public class DatabaseSeederService {
     private VeterinarySeederService veterinarySeederService;
     private AirportSeederService airportSeederService;
     private TicketBusSeederService ticketBusSeederService;
+    private TransitTaxesSeederService transitTaxesSeederService;
 
     @Autowired
     public DatabaseSeederService(ShopSeederService shopSeederService, SchoolSeederService schoolSeederService,
@@ -40,7 +42,7 @@ public class DatabaseSeederService {
                                  SportCentreSeederService sportCentreSeederService, KitchenSeederService kitchenSeederService,
                                  ProjectSeederService projectSeederService, HospitalSeederService hospitalSeederService,
                                  VeterinarySeederService veterinarySeederService, AirportSeederService airportSeederService,
-                                 TicketBusSeederService ticketBusSeederService) {
+                                 TicketBusSeederService ticketBusSeederService, TransitTaxesSeederService transitTaxesSeederService) {
         this.shopSeederService = shopSeederService;
         this.schoolSeederService = schoolSeederService;
         this.factorySeederService = factorySeederService;
@@ -54,6 +56,7 @@ public class DatabaseSeederService {
         this.veterinarySeederService = veterinarySeederService;
         this.airportSeederService = airportSeederService;
         this.ticketBusSeederService = ticketBusSeederService;
+        this.transitTaxesSeederService = transitTaxesSeederService;
         this.seedDatabase();
     }
 
@@ -71,6 +74,7 @@ public class DatabaseSeederService {
         this.veterinarySeederService.seedDatabase();
         this.airportSeederService.seedDataBase();
         this.ticketBusSeederService.seedDatabase();
+        this.transitTaxesSeederService.seedDatabase();
     }
 
     public void deleteAll() {
@@ -87,6 +91,7 @@ public class DatabaseSeederService {
         this.veterinarySeederService.deleteAll();
         this.airportSeederService.deleteAll();
         this.ticketBusSeederService.deleteAll();
+        this.transitTaxesSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
