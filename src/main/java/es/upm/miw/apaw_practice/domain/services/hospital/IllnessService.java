370cfc40e333bbbca47dadac7fe813adda492b5c
Andrea <drealvarez97@gmail.com>
2020-10-06 20:18:46 +0200

    Added PATCH and test of Illness #52

--- src/main/java/es/upm/miw/apaw_practice/domain/services/hospital/IllnessService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/hospital/IllnessService.java
@@ -1,10 +1,12 @@
 package es.upm.miw.apaw_practice.domain.services.hospital;
 
 import es.upm.miw.apaw_practice.domain.models.hospital.Illness;
+import es.upm.miw.apaw_practice.domain.models.hospital.IllnessContagiousUpdating;
 import es.upm.miw.apaw_practice.domain.persistence_ports.hospital.IllnessPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @Service
@@ -18,5 +20,11 @@ public class IllnessService {
 
     public Stream<Illness> readAll(){
         return this.illnessPersistence.readAll();
+    }
+
+    public void updateContagious(IllnessContagiousUpdating illnessContagiousUpdating) {
+            this.illnessPersistence.readByPhase(illnessContagiousUpdating.getPhase())
+                    .peek(illness -> illness.setContagious(illnessContagiousUpdating.getContagious()))
+                    .forEach(illness -> this.illnessPersistence.update(illness));
     }
 }
