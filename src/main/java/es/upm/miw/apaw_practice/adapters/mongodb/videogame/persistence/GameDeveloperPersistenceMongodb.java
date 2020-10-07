f9f1407fd1afe5506ed6a798dbe44c3e1d637b88
nataliacasanova <n.casanova@alumnos.upm.es>
2020-10-07 20:16:41 +0200

    #134 added POST GameDeveloper and test

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/videogame/persistence/GameDeveloperPersistenceMongodb.java
@@ -0,0 +1,42 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.videogame.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.videogame.daos.GameDeveloperRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.videogame.entities.GameDeveloperEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.ConflictException;
+import es.upm.miw.apaw_practice.domain.models.videogame.GameDeveloper;
+import es.upm.miw.apaw_practice.domain.models.videogame.GameDeveloperCreation;
+import es.upm.miw.apaw_practice.domain.persistence_ports.videogame.GameDeveloperPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("gameDeveloperPersistence")
+public class GameDeveloperPersistenceMongodb implements GameDeveloperPersistence {
+
+    private GameDeveloperRepository gameDeveloperRepository;
+
+    @Autowired
+    public GameDeveloperPersistenceMongodb(GameDeveloperRepository gameDeveloperRepository){
+        this.gameDeveloperRepository = gameDeveloperRepository;
+    }
+
+    @Override
+    public GameDeveloper create(GameDeveloperCreation gameDeveloperCreation) {
+
+        this.assertNameNotExist(gameDeveloperCreation.getName());
+        return this.gameDeveloperRepository
+                .save(new GameDeveloperEntity(gameDeveloperCreation))
+                .toGameDeveloper();
+    }
+
+    public void assertNameNotExist(String name){
+        this.gameDeveloperRepository
+                .findByName(name)
+                .ifPresent(gameDeveloper -> {
+                    throw new ConflictException("Name exist: " +gameDeveloper);
+                });
+    }
+
+
+
+
+}
