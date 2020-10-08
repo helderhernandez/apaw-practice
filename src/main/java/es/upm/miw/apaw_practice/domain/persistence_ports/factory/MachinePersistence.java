0f176ee5a34ef144758dafcafc4dd9d340c16fe8
Andrea Calvo <andreac.3108@gmail.com>
2020-10-08 13:12:35 +0200

    change method input name #129

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/factory/MachinePersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/factory/MachinePersistence.java
@@ -5,7 +5,7 @@ import es.upm.miw.apaw_practice.domain.models.factory.Machine;
 import java.util.List;
 
 public interface MachinePersistence {
-    Machine updateStatus(String id, Boolean isActive);
+    Machine updateStatus(String id, Boolean active);
 
     List<Machine> findMachineByEmployeeDegreeTitle(String title);
 
