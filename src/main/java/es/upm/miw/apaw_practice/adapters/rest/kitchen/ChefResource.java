7297d79d964d9d7df4cbaf31993b2b3bb8f5eab4
MRxRafi <rafadriveclase@gmail.com>
2020-10-08 09:50:25 +0200

    PUT completed. Testing needed #65

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/kitchen/ChefResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/kitchen/ChefResource.java
@@ -1,15 +1,19 @@
 package es.upm.miw.apaw_practice.adapters.rest.kitchen;
 
 import es.upm.miw.apaw_practice.domain.models.kitchen.Chef;
+import es.upm.miw.apaw_practice.domain.services.kitchen.ChefService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(ChefResource.CHEF)
 public class ChefResource {
     static final String CHEF = "/kitchen/chef";
     static final String DNI = "/{dni}";
+    static final String RECIPES_FINISHED = "/recipesFinished";
 
     private ChefService chefService;
 
@@ -18,9 +22,9 @@ public class ChefResource {
         this.chefService = chefService;
     }
 
-    @PutMapping(DNI)
-    public Chef updateName(@PathVariable String dni, @PathVariable String name) {
-        return this.chefService.updateName(dni, name);
+    @PutMapping(DNI + RECIPES_FINISHED)
+    public Chef updateRecipesFinished(@PathVariable ChefDto chefDto) {
+        return this.chefService.updateRecipesFinished(chefDto.getDni(), chefDto.getRecipesFinished());
     }
 
 }
