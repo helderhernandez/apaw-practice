87a2030ee6336898c3c3a8d8e27bb029b51a37ce
lihao1214 <hao.li@alumnos.upm.es>
2020-10-09 11:54:00 +0200

    Add Music Entity&Test #23

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/music/entities/StyleEntity.java
@@ -0,0 +1,46 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.music.entities;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class StyleEntity {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    public  StyleEntity(){
+        //Empty for framework
+    }
+
+    public StyleEntity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+
+
+    @Override
+    public String toString() {
+        return "StyleEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
