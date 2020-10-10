6bc1a561f4e7a8f38ddb9082d94c38c0371c47f3
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-10-10 12:57:19 +0200

    Create and implement findByName method in persistence and repository #194

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/filmforum/persistence/FilmForumPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/filmforum/persistence/FilmForumPersistenceMongodb.java
@@ -1,2 +1,18 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.filmforum.persistence;public class FilmForumPersistenceMongodb {
+package es.upm.miw.apaw_practice.adapters.mongodb.filmforum.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.filmforum.daos.FilmForumRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.filmforum.entities.FilmForumEntity;
+import es.upm.miw.apaw_practice.domain.models.filmforum.FilmForum;
+import es.upm.miw.apaw_practice.domain.persistence_ports.filmforum.FilmForumPersistence;
+
+public class FilmForumPersistenceMongodb implements FilmForumPersistence {
+
+    FilmForumRepository filmForumRepository;
+
+    @Override
+    public FilmForum findByName(String name) {
+        return filmForumRepository.findByName(name)
+                .map(FilmForumEntity::toFilmForum)
+                .orElse(null);
+    }
 }
--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/filmforum/FilmForumPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/filmforum/FilmForumPersistence.java
@@ -2,8 +2,6 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.filmforum;
 
 import es.upm.miw.apaw_practice.domain.models.filmforum.FilmForum;
 
-import java.util.List;
-
 public interface FilmForumPersistence {
-    List<FilmForum> findByName(String name);
+    FilmForum findByName(String name);
 }
