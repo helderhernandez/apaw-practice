f7e5e1495da7fef416e5e77ccdbf9e00e23cec70
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-10-10 13:14:30 +0200

    Add findByName to filmForum service. #194

--- src/main/java/es/upm/miw/apaw_practice/domain/services/filmforum/FilmForumService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/filmforum/FilmForumService.java
@@ -2,7 +2,9 @@ package es.upm.miw.apaw_practice.domain.services.filmforum;
 
 import es.upm.miw.apaw_practice.domain.models.filmforum.FilmForum;
 import es.upm.miw.apaw_practice.domain.persistence_ports.filmforum.FilmForumPersistence;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FilmForumService {
     private FilmForumPersistence filmForumPersistence;
 
