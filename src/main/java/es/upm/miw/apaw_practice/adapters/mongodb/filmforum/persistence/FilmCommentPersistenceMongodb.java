79dfcaa53ed1f3164442c7cfe96c8a6725b2ab4f
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-10-12 17:44:56 +0200

    Add DELETE comment #229

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/filmforum/persistence/FilmCommentPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/filmforum/persistence/FilmCommentPersistenceMongodb.java
@@ -1,4 +1,25 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.filmforum.persistence;
 
-public class FilmCommentPersistenceMongodb {
+import es.upm.miw.apaw_practice.adapters.mongodb.filmforum.daos.FilmCommentRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.filmforum.entities.FilmCommentEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.persistence_ports.filmforum.FilmCommentPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("filmCommentPersistence")
+public class FilmCommentPersistenceMongodb implements FilmCommentPersistence {
+
+    private FilmCommentRepository filmCommentRepository;
+
+    public FilmCommentPersistenceMongodb(FilmCommentRepository filmCommentRepository) {
+        this.filmCommentRepository = filmCommentRepository;
+    }
+
+    @Override
+    public void delete(String id) {
+        FilmCommentEntity comment = filmCommentRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Comment with id " + id + " not found."));
+        filmCommentRepository.delete(comment);
+    }
 }
