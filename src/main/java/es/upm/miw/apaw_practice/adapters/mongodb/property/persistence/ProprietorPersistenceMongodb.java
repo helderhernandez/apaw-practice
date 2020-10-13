239285d7be870482b5dc05f97bf0927b1529187a
Patricia403 <736026544@qq.com>
2020-10-13 19:16:05 +0200

    Added PATCH of Property #144

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/property/persistence/ProprietorPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/property/persistence/ProprietorPersistenceMongodb.java
@@ -1,6 +1,8 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.property.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.property.daos.ProprietorRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.property.entities.ProprietorEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.property.Proprietor;
 import es.upm.miw.apaw_practice.domain.persistence_ports.property.ProprietorPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,25 @@ public class ProprietorPersistenceMongodb implements ProprietorPersistence {
     public Stream<Proprietor> readAll() {
         return this.proprietorRepository.findAll().stream()
                 .map(proprietorEntity -> proprietorEntity.toProprietor());
+    }
+
+    //@Override
+    public Proprietor update(Proprietor proprietor) {
+        ProprietorEntity proprietorEntity = this.proprietorRepository
+                .findByDni(proprietor.getDni())
+                .orElseThrow(() -> new NotFoundException("Proprietor dni: " + proprietor.getDni()));
+        proprietorEntity.fromProprietor(proprietor);
+        return this.proprietorRepository
+                .save(proprietorEntity)
+                .toProprietor();
+    }
+
+    //@Override
+    public Proprietor readByDni(String dni) {
+        return this.proprietorRepository
+                .findByDni(dni)
+                .orElseThrow(() -> new NotFoundException("Student dni: " + dni))
+                .toProprietor();
     }
 }
 
