b727d6463804930128f7807282f3f8726b93e0fa
Patricia403 <736026544@qq.com>
2020-10-13 20:10:45 +0200

    Added DELETE of Property #145

--- src/main/java/es/upm/miw/apaw_practice/domain/services/property/MaintanenceItemService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/property/MaintanenceItemService.java
@@ -1,4 +1,19 @@
 package es.upm.miw.apaw_practice.domain.services.property;
 
+import es.upm.miw.apaw_practice.domain.persistence_ports.property.MaintanenceItemPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MaintanenceItemService {
+    private final MaintanenceItemPersistence maintanenceItemPersistence;
+
+    @Autowired
+    public MaintanenceItemService(MaintanenceItemPersistence maintanenceItemPersistence) {
+        this.maintanenceItemPersistence = maintanenceItemPersistence;
+    }
+
+    public void delete(String id) {
+        this.maintanenceItemPersistence.deleteById(id);
+    }
 }
