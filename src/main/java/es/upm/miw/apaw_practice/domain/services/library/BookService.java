1a408144b709342526ec1f75b9e6acdc2be3dfcb
Sebastian <wangjiaxin6667@gmail.com>
2020-10-15 22:54:06 +0200

    Patch Book #239

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/library/BookService.java
@@ -0,0 +1,30 @@
+package es.upm.miw.apaw_practice.domain.services.library;
+
+import es.upm.miw.apaw_practice.domain.models.library.Book;
+import es.upm.miw.apaw_practice.domain.models.library.BookPriceUpdating;
+import es.upm.miw.apaw_practice.domain.persistence_ports.library.BookPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private BookPersistence bookPersistence;
+
+    @Autowired
+    public BookService(BookPersistence bookPersistence){
+        this.bookPersistence=bookPersistence;
+    }
+
+    public void updateBookPrice(List<BookPriceUpdating> bookPriceUpdatingList){
+        bookPriceUpdatingList.stream()
+                .map(BookNewPrice ->{
+                    Book book=this.bookPersistence.readByISBN(BookNewPrice.getISBN());
+                    book.setPrice(BookNewPrice.getPrice());
+                    return book;
+                })
+                .forEach(book -> this.bookPersistence.updateBookPrice(book));
+    }
+}
