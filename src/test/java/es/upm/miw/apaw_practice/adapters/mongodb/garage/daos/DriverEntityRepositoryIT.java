8f57b317c3ba15481c70b0786378100038f0e4c3
Miguel Garcia <miguelgarse@gmail.com>
2020-10-15 21:31:03 +0200

    Created Builder to build a Driver object with its tests #286

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/garage/daos/DriverEntityRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/garage/daos/DriverEntityRepositoryIT.java
@@ -2,8 +2,12 @@ package es.upm.miw.apaw_practice.adapters.mongodb.garage.daos;
 
 import es.upm.miw.apaw_practice.TestConfig;
 import es.upm.miw.apaw_practice.adapters.mongodb.garage.entities.DriverEntity;
+import es.upm.miw.apaw_practice.domain.models.garage.Driver;
+import es.upm.miw.apaw_practice.domain.models.garage.Vehicle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +26,29 @@ public class DriverEntityRepositoryIT {
         assertEquals("frcgon@example.com", driver.getEmail());
         assertEquals(Boolean.FALSE, driver.getGarageMember());
         assertFalse(driver.getVehicleEntities().isEmpty());
+    }
+
+    @Test
+    void testDriverBuilder() {
+        Driver driver = Driver.builder()
+                .id("12345")
+                .dni("54685214G")
+                .name("Alberto")
+                .telephone("924658999")
+                .email("test@example.com")
+                .garageMember(Boolean.TRUE)
+                .vehicles(List.of(new Vehicle()))
+                .build();
+
+        assertNotNull(driver);
+
+        assertEquals("12345", driver.getId());
+        assertEquals("54685214G", driver.getDni());
+        assertEquals("Alberto", driver.getName());
+        assertEquals("924658999", driver.getTelephone());
+        assertEquals("test@example.com", driver.getEmail());
+        assertEquals(Boolean.TRUE, driver.getGarageMember());
+        assertEquals(1, driver.getVehicles().size());
     }
 
 }
