3cce807e19df384f017a4ac8a92f78d9487b4888
Daniel Rosado <danieljesus.rosado@alumnos.upm.es>
2020-10-16 05:19:07 +0200

    Add PATCH Runner endpoint and tests #211

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/race/daos/RunnerRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/race/daos/RunnerRepository.java
@@ -3,5 +3,10 @@ package es.upm.miw.apaw_practice.adapters.mongodb.race.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.race.entities.RunnerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface RunnerRepository extends MongoRepository<RunnerEntity, String> {
+
+    Optional<RunnerEntity> findByDni(String dni);
+
 }
