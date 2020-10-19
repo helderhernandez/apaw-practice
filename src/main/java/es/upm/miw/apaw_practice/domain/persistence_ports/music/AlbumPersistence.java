217474b83973a59d61dbca02134c9faecc893a69
lihao1214 <hao.li@alumnos.upm.es>
2020-10-19 23:39:55 +0200

    ADD Search 1 #329

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/music/AlbumPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/music/AlbumPersistence.java
@@ -1,5 +1,10 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.music;
 
+import es.upm.miw.apaw_practice.domain.models.music.Album;
+
+import java.math.BigDecimal;
+
 public interface AlbumPersistence {
     void deleteById(String id);
+    BigDecimal findTotalPriceAlbumByStyleName(String name);
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/music/AlbumResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/music/AlbumResourceIT.java
@@ -2,9 +2,14 @@
 package es.upm.miw.apaw_practice.adapters.rest.music;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.music.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RestTestConfig
 public class AlbumResourceIT {
@@ -18,5 +23,19 @@ public class AlbumResourceIT {
                 .uri(AlbumResource.ALBUMS + AlbumResource.ID_ID,"kk")
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    void testFindTotalPriceAlbumByStyleName(){
+        this.webTestClient
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path(AlbumResource.ALBUMS + AlbumResource.SEARCH)
+                        .queryParam("q","name:POP")
+                        .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(BigDecimal.class)
+                .value(album ->  assertEquals(new BigDecimal("59.97"),album.get(0)));
     }
 }
