81c4e288805c36f67faacacd2f3b5db513a48a58
Luis Conde <lconder@MacBookPro.local>
2021-01-13 11:27:33 -0600

    Patch Appointment issue#378

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/studio/AppointmentPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/studio/AppointmentPersistence.java
@@ -2,4 +2,6 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.studio;
 
 public interface AppointmentPersistence {
     void deleteById(String id);
+
+    void updateIsActiveFlags(Boolean isActive);
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/studio/AppointmentResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/studio/AppointmentResourceIT.java
@@ -4,6 +4,7 @@ import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @RestTestConfig
 public class AppointmentResourceIT {
@@ -16,6 +17,16 @@ public class AppointmentResourceIT {
         this.webTestClient
                 .delete()
                 .uri(AppointmentResource.APPOINTMENT + AppointmentResource.ID, "kk")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void testUpdateIsActiveFlags() {
+        this.webTestClient
+                .patch()
+                .uri(AppointmentResource.APPOINTMENT)
+                .body(BodyInserters.fromValue(false))
                 .exchange()
                 .expectStatus().isOk();
     }
