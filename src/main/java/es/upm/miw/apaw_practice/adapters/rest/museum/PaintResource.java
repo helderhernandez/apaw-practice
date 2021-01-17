b58f9da689bd24c99fc7699ed41e6d08695729e7
Walter Aldas <walerical18@hotmail.com>
2021-01-17 21:07:54 +0100

    issue#387 testFindPaintCollectionByArtRestorerSurname method Added!

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/museum/PaintResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/museum/PaintResource.java
@@ -1,9 +1,13 @@
 package es.upm.miw.apaw_practice.adapters.rest.museum;
 
+import es.upm.miw.apaw_practice.adapters.rest.LexicalAnalyzer;
+import es.upm.miw.apaw_practice.domain.models.museum.Artist;
 import es.upm.miw.apaw_practice.domain.models.museum.Paint;
 import es.upm.miw.apaw_practice.domain.services.museum.PaintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(PaintResource.PAINTS)
@@ -11,6 +15,7 @@ public class PaintResource {
 
     static final String PAINTS = "/musemum/paints";
     static final String ID_ID = "/{id}";
+    static final String SEARCH = "/search";
 
     private PaintService paintService;
 
@@ -27,6 +32,12 @@ public class PaintResource {
     @DeleteMapping(ID_ID)
     public void delete(@PathVariable String id) {
         this.paintService.delete(id);
+    }
+
+    @GetMapping(SEARCH)
+    public Stream<Paint> findPaintCollectionByArtRestorerSurname(@RequestParam String q) {
+        String artRestorerSurname = new LexicalAnalyzer().extractWithAssure(q, "surname");
+        return this.paintService.findPaintCollectionByArtRestorerSurname(artRestorerSurname);
     }
 
 
--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/museum/PaintPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/museum/PaintPersistence.java
@@ -3,11 +3,15 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.museum;
 import es.upm.miw.apaw_practice.domain.models.museum.Paint;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface PaintPersistence {
 
     Paint create(Paint paint);
 
     void deleteById(String id);
+
+    Stream<Paint> findPaintCollectionByArtRestorerSurname(String artRestorerSurname);
 
 }
